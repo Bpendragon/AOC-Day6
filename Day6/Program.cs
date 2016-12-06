88163cef7b3c21f9641bc25b4354c9a17dd74c25
Bpendragon (David Camp) <david@campclan.net>
2016-12-06 03:10:58 -0800

    Actually put in the code. It's good stuff.

--- Day6/Program.cs
+++ Day6/Program.cs
@@ -10,13 +10,46 @@ namespace Day6
 	{
 		static void Main(string[] args)
 		{
-			System.IO.StreamReader file = new System.IO.StreamReader("input.txt");
+			//flip this to change the output from one half of the problem to the other
+			bool Part1 = false;
+			System.IO.StreamReader file = new System.IO.StreamReader("..\\..\\input.txt");
 			string line;
+
+			var data = new Dictionary<int, Dictionary<char, int>>();
+
+			for (int i = 0; i < 8; i++)
+			{
+				data.Add(i, new Dictionary<char, int>());
+			}
 
 			while ((line = file.ReadLine()) != null)
 			{
-
+				for (int i = 0; i < 8; i++)
+				{
+					char curChar = line.ElementAt(i);
+					if(data[i].ContainsKey(curChar))
+					{
+						data[i][curChar]++;
+					} else
+					{
+						data[i].Add(curChar, 0);
+					}
+				}
 			}
+
+			for (int i = 0; i < 8; i++)
+			{
+				if (Part1)
+				{
+					Console.Write(data[i].ToList().OrderByDescending(x => x.Value).First().Key);
+				} else
+				{
+					Console.Write(data[i].ToList().OrderBy(x => x.Value).First().Key);
+				}
+			}
+			Console.WriteLine();
+			Console.WriteLine("Press any key to finish");
+			Console.ReadKey();
 		}
 	}
 }
